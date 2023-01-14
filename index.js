6edea901cc21f4b1251204838455746cd93aa489
Aifaz Karedia <70049305+aifaz23@users.noreply.github.com>
2023-01-13 23:06:07 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+function contactDone() {
+    alert("We received your questions. \nA member of our team will get back to you as sooon as possible.");
+}
+
+function appointDone() {
+    alert("We received your request. \nWe will get back to you with further details.");
+}
